da132b8da9c2c42a9c8ce31e3ffd610a4726a299
my-first-repo <saiyadul77@gmail.com>
2022-04-22 17:47:28 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,9 @@
+const works = ['module unclock at 8pm', 'practice after watch the video', 'collect note to watch video', 'own practice after finishing module', 'joined support session if do not understand any topics'];
+/* let i = 1;
+while (i <= 10) {
+    console.log(works);
+    i++;
+} */
+for (let i = 1; i <= 10; i++) {
+    console.log(works);
+}
